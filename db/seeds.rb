bfaec7735c7c65e0a8ea814afaba460073dffe5b
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 16:34:06 -0300

    improve the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+puts "Destroy all restaurants"
+Restaurant.destroy_all
+
 puts "Create some Restaurants"
 
-Restaurant.create! name: "Amareleen", address: "Gloria"
-Restaurant.create! name: "Verdeen", address: "Gloria"
+Restaurant.create! name: "Amareleen", neighborhood: "Gloria", city: "Rio de Janeiro"
+Restaurant.create! name: "Verdeen", neighborhood: "Gloria", city: "Rio de Janeiro"
+Restaurant.create! name: "Sushi da Praça", neighborhood: "Recreio dos Bandeirantes", city: "Rio de Janeiro"
+Restaurant.create! name: "Vikings", neighborhood: "Tijuca", city: "Rio de Janeiro"
+Restaurant.create! name: "Forneria Gagliasso", neighborhood: "Barra da Tijuca", city: "Rio de Janeiro"
+Restaurant.create! name: "A Casa do Ferreiro", neighborhood: "Vila Izabel", city: "Curitiba"
 
 puts "Let's drink a beer"
 puts %{
