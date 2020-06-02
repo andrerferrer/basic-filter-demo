3301d3204574c01c589c9c4c09fb28d344d61a16
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-02 16:34:21 -0300

    Implement the filter logic in the controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,24 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all
+  	# Start with all the restaurants
+	@restaurants = Restaurant.all
+
+	# Filter if we have a filter
+	filter_restaurants if params[:search]
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
     @review = Review.new
+  end
+
+  private
+
+  def filter_restaurants
+ 	search = params[:search]
+ 	# The below allows searching for 'amarel' to return amareleen
+ 	@restaurants = Restaurant.where('name ILIKE ?', "%#{search[:name]}%") unless search[:name].empty?
+ 	# The bellow filters by neighborhood
+ 	@restaurants = Restaurant.where(neighborhood: search[:neighborhood]) unless search[:neighborhood].empty?
   end
 end
