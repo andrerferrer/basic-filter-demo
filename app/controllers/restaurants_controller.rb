9fa6d8bc44317e0f12e22a182ae6f9a02d47c8a6
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-04 10:38:42 -0300

    INDEEEENT

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,10 @@
 class RestaurantsController < ApplicationController
   def index
-  	# Start with all the restaurants
-	@restaurants = Restaurant.all
+    # Start with all the restaurants
+    @restaurants = Restaurant.all
 
-	# Filter if we have a filter
-	filter_restaurants if params[:search]
+    # Filter if we have a filter
+    filter_restaurants if params[:search]
   end
 
   def show
@@ -15,10 +15,10 @@ class RestaurantsController < ApplicationController
   private
 
   def filter_restaurants
- 	search = params[:search]
- 	# The below allows searching for 'amarel' to return amareleen
- 	@restaurants = Restaurant.where('name ILIKE ?', "%#{search[:name]}%") unless search[:name].empty?
- 	# The bellow filters by neighborhood
- 	@restaurants = Restaurant.where(neighborhood: search[:neighborhood]) unless search[:neighborhood].empty?
+    search = params[:search]
+    # The below allows searching for 'amarel' to return amareleen
+    @restaurants = Restaurant.where('name ILIKE ?', "%#{search[:name]}%") unless search[:name].empty?
+    # The bellow filters by neighborhood
+    @restaurants = Restaurant.where(neighborhood: search[:neighborhood]) unless search[:neighborhood].empty?
   end
 end
